4a4ba87388fc8f1a501c4796f280e46d642faead
Luis Aceituno <aceitunosoriano@gmail.com>
2024-12-30 22:33:22 +0100

    update history size for zsh

--- zsh/.zsh_config/history.zsh
+++ zsh/.zsh_config/history.zsh
@@ -1,4 +1,4 @@
 # History in cache directory:
 HISTSIZE=10000                # Set the maximum number of commands to remember in the command history
-SAVEHIST=10000                # Set the maximum number of history events to save in the history file
+SAVEHIST=50000                # Set the maximum number of history events to save in the history file
 HISTFILE=~/.cache/zsh/history # Set the file to save history

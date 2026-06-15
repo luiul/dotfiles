46d12041378ef3272955c0b00d17f69a7e1b4f8a
luiul <aceitunosoriano@gmail.com>
2026-06-15 13:10:32 +0200

    feat(zsh): source cargo env to add Rust to PATH

--- zsh/.zsh_config/path.zsh
+++ zsh/.zsh_config/path.zsh
@@ -1,2 +1,5 @@
 # System PATH Configuration
 export PATH="$HOME/.local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+
+# Rust / Cargo (adds $HOME/.cargo/bin to PATH)
+[[ -f "$HOME/.cargo/env" ]] && source "$HOME/.cargo/env"
